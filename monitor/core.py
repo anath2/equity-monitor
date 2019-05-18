2605fb588fa6496bdb9f4a1777f4695fbf2dba19
Ajit Nath <nath.ajit01@gmail.com>
2019-05-18 15:07:33 +0800

    adds modules for retrieving equity data

---
+++ monitor/core.py
@@ -0,0 +1,21 @@
+'''
+core.py
+-------
+Functions quantitative analyis and retrieving equity data
+'''
+
+from . import scrape
+
+
+def get_current_price(ticker: str) -> float:
+    '''
+    Gets current price from ticker
+    '''
+    pass
+
+
+def get_current_volume(ticker: str) -> int:
+    '''
+    Gets current volume from ticker
+    '''
+    pass
---
+++ monitor/scrape.py
@@ -0,0 +1,34 @@
+'''
+scrape.py
+---------
+'''
+
+
+class Yahoo:
+    '''
+    Retrieves equity data from http:://finance.yahoo.com
+    '''
+
+    def __init__(self):
+        pass
+
+    def price(self, ticker):
+        pass
+
+    def volume(self, ticker):
+        pass
+
+
+class Google:
+    '''
+    Retrieves equity data from http:://finance.google.com
+    '''
+
+    def __init__(self):
+        pass
+
+    def price(self, ticker):
+        pass
+
+    def volume(self, ticker):
+        pass
