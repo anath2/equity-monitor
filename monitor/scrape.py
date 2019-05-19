f0cd0d92dff032a127433e9c9c221ec9790c4d0f
Ajit Nath <nath.ajit01@gmail.com>
2019-05-19 19:16:08 +0800

    Retrieves price table from yahoo

--- monitor/scrape.py
+++ monitor/scrape.py
@@ -3,20 +3,49 @@ scrape.py
 ---------
 '''
 
+import requests
+import lxml
+from lxml import html
+import pandas as pd
+
+
+class ScrapeException(Exception):
+    '''
+    Raise when an error occurs scraping financial data
+    '''
+
 
 class Yahoo:
     '''
     Retrieves equity data from http:://finance.yahoo.com
     '''
 
-    def __init__(self):
-        pass
+    URL_STRING = "http://finance.yahoo.com/quote/%s?p=%s"
 
-    def price(self, ticker):
-        pass
+    def __init__(self, symbol: str):
+        self.url = self.URL_STRING % (symbol, symbol)
 
-    def volume(self, ticker):
-        pass
+        try:
+    `       contents = requests.get(self.url).content
+        except requests.exceptions.RequestException as err:
+            raise ScrapeException(err)
+            
+        html_data = html.fromstring(contents)
+        table_list = html_data.xpath("//table")
+
+        if not table_list:
+            raise ScrapeException("No data for Symbol - {} on Yahoo".format(symbol))
+
+        table_string = lxml.etree.tostring(table_list[0], method='lxml')
+        price_table = pd.read_html(table_string)[0].transpose()  # Formats table
+        price_table.columns = price_table.iloc[0, :]
+        self.price_table = price_table.iloc[1:, :]
+
+    def price(self) -> float:
+        return float(self.price_table.loc['Close'])
+
+    def volume(self) -> int:
+        return int(self.price_table.loc['Volume'])
 
 
 class Google:
@@ -27,8 +56,8 @@ class Google:
     def __init__(self):
         pass
 
-    def price(self, ticker):
+    def price(self):
         pass
 
-    def volume(self, ticker):
+    def volume(self):
         pass
